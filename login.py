a2a2e0523e5008b970bcd4a7e6b59e4fb7e4c893
felipefln <felipecarvalho_22_@hotmail.com>
2018-07-26 21:10:08 -0300

    Tela de Login

--- login.py
+++ login.py
@@ -1,82 +1,82 @@
 from tkinter import *
 import os
  
-users = 'tempfile.temp' # This just sets the variable creds to 'tempfile.temp'
+users = 'tempfile.temp' # Arquivo para armazenar usuario
  
-def Signup(): # This is the signup definition, 
-    global pwordE # These globals just make the variables global to the entire script, meaning any definition can use them
+def Signup(): # Metodo para criar usuário, 
+    global pwordE # Variaveis globais sendo declaradas
     global nameE
     global roots
  
-    roots = Tk() # This creates the window, just a blank one.
-    roots.title('Criar Usuário') # This renames the title of said window to 'signup'
-    intruction = Label(roots, text='Insira suas Informações\n') # This puts a label, so just a piece of text saying 'please enter blah'
-    intruction.grid(row=0, column=0, sticky=E) # This just puts it in the window, on row 0, col 0. If you want to learn more look up a tkinter tutorial :)
+    roots = Tk() 
+    roots.title('Criar Usuário') 
+    intruction = Label(roots, text='Insira suas Informações\n')
+    intruction.grid(row=0, column=0, sticky=E) 
  
-    nameL = Label(roots, text='Novo Usuário: ') # This just does the same as above, instead with the text new username.
-    pwordL = Label(roots, text='Nova Senha: ') # ^^
-    nameL.grid(row=1, column=0, sticky=W) # Same thing as the instruction var just on different rows. :) Tkinter is like that.
-    pwordL.grid(row=2, column=0, sticky=W) # ^^
+    nameL = Label(roots, text='Novo Usuário: ') 
+    pwordL = Label(roots, text='Nova Senha: ') 
+    nameL.grid(row=1, column=0, sticky=W) 
+    pwordL.grid(row=2, column=0, sticky=W) 
  
-    nameE = Entry(roots) # This now puts a text box waiting for input.
-    pwordE = Entry(roots, show='*') # Same as above, yet 'show="*"' What this does is replace the text with *, like a password box :D
-    nameE.grid(row=1, column=1) # You know what this does now :D
-    pwordE.grid(row=2, column=1) # ^^
+    nameE = Entry(roots)
+    pwordE = Entry(roots, show='*')
+    nameE.grid(row=1, column=1) 
+    pwordE.grid(row=2, column=1)
  
-    signupButton = Button(roots, text='Criar', command=FSSignup) # This creates the button with the text 'signup', when you click it, the command 'fssignup' will run. which is the def
+    signupButton = Button(roots, text='Criar', command=FSSignup) 
     signupButton.grid(columnspan=3, sticky=W)
-    roots.mainloop() # This just makes the window keep open, we will destroy it soon
+    roots.mainloop() 
  
-def FSSignup():
-    with open(users, 'w') as f: # Creates a document using the variable we made at the top.
-        f.write(nameE.get()) # nameE is the variable we were storing the input to. Tkinter makes us use .get() to get the actual string.
-        f.write('\n') # Splits the line so both variables are on different lines.
-        f.write(pwordE.get()) # Same as nameE just with pword var
-        f.close() # Closes the file
+def FSSignup(): #Metodo que abri, salva e fecha o arquivo users
+    with open(users, 'w') as f: 
+        f.write(nameE.get()) .
+        f.write('\n') 
+        f.write(pwordE.get()) 
+        f.close() 
  
-    roots.destroy() # This will destroy the signup window. :)
-    Login() # This will move us onto the login definition :D
+    roots.destroy() 
+    Login() 
  
-def Login():
+def Login(): #Metodo para criação da tela Login
     global nameEL
-    global pwordEL # More globals :D
+    global pwordEL 
     global rootA
  
-    rootA = Tk() # This now makes a new window.
-    rootA.title('Login') # This makes the window title 'login'
+    rootA = Tk() 
+    rootA.title('Login') 
  
-    intruction = Label(rootA, text='Login\n') # More labels to tell us what they do
-    intruction.grid(sticky=E) # Blahdy Blah
+    intruction = Label(rootA, text='Login\n') 
+    intruction.grid(sticky=E) 
  
-    nameL = Label(rootA, text='Usuário: ') # More labels
-    pwordL = Label(rootA, text='Senha: ') # ^
+    nameL = Label(rootA, text='Usuário: ')
+    pwordL = Label(rootA, text='Senha: ') 
     nameL.grid(row=1, sticky=W)
     pwordL.grid(row=2, sticky=W)
  
-    nameEL = Entry(rootA) # The entry input
+    nameEL = Entry(rootA) 
     pwordEL = Entry(rootA, show='*')
     nameEL.grid(row=1, column=1)
     pwordEL.grid(row=2, column=1)
  
-    loginB = Button(rootA, text='Logar no Sistema', command=CheckLogin) # This makes the login button, which will go to the CheckLogin def.
+    loginB = Button(rootA, text='Logar no Sistema', command=CheckLogin) 
     loginB.grid(columnspan=2, sticky=W)
  
-    rmuser = Button(rootA, text='Excluir Usuário', fg='red', command=DelUser) # This makes the deluser button. blah go to the deluser def.
+    rmuser = Button(rootA, text='Excluir Usuário', fg='red', command=DelUser) 
     rmuser.grid(columnspan=2, sticky=W)
     rootA.mainloop()
  
-def CheckLogin():
+def CheckLogin(): #Metodo 
     with open(users) as f:
-        data = f.readlines() # This takes the entire document we put the info into and puts it into the data variable
-        uname = data[0].rstrip() # Data[0], 0 is the first line, 1 is the second and so on.
-        pword = data[1].rstrip() # Using .rstrip() will remove the \n (new line) word from before when we input it
+        data = f.readlines() 
+        uname = data[0].rstrip() 
+        pword = data[1].rstrip() 
  
-    if nameEL.get() == uname and pwordEL.get() == pword: # Checks to see if you entered the correct data.
-        r = Tk() # Opens new window
+    if nameEL.get() == uname and pwordEL.get() == pword: 
+        r = Tk() 
         r.title(':D')
-        r.geometry('300x100') # Makes the window a certain size
-        rlbl = Label(r, text='\n[+] Usuário Logado') # "logged in" label
-        rlbl.pack() # Pack is like .grid(), just different
+        r.geometry('300x100') 
+        rlbl = Label(r, text='\n[+] Usuário Logado')
+        rlbl.pack() 
         r.mainloop()
     else:
         r = Tk()
@@ -87,11 +87,11 @@ def CheckLogin():
         r.mainloop()
  
 def DelUser():
-    os.remove(users) # Removes the file
-    rootA.destroy() # Destroys the login window
-    Signup() # And goes back to the start!
+    os.remove(users) 
+    rootA.destroy() 
+    Signup() 
  
 if os.path.isfile(users):
     Login()
-else: # This if else statement checks to see if the file exists. If it does it will go to Login, if not it will go to Signup :)
+else:
     Signup()
