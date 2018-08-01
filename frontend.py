fd028b89e060ad519b7faf97c4b1f6974a07d8c9
felipefln <felipecarvalho_22_@hotmail.com>
2018-08-01 15:28:28 -0300

    Aplicação frontend que chamará o backend para tratar os dados, armazena-los, e mostra-los para o usuário.

--- frontend.py
+++ frontend.py
@@ -1,19 +1,19 @@
 from tkinter import *
-import backend
+import backend as bck
+
 
 window=Tk()
-connect()
 
-l1 = Label(window, tex="Title")
+l1 = Label(window, text="Title")
 l1.grid(row=0, column=0)
 
-l2 = Label(window, tex="Author")
+l2 = Label(window, text="Author")
 l2.grid(row=0, column=2)
 
-l3 = Label(window, tex="Year")
+l3 = Label(window, text="Year")
 l3.grid(row=1, column=0)
 
-l4 = Label(window, tex="Prince")
+l4 = Label(window, text="Prince")
 l4.grid(row=1, column=2)
 
 
@@ -29,35 +29,36 @@ year_text=StringVar()
 e3=Entry(window, textvariable=year_text)
 e3.grid(row=1, column=1)
 
-isbn_text=StringVar()
+prince_text=StringVar()
 e4=Entry(window, textvariable=prince_text)
 e4.grid(row=1, column=3)
 
 
-list1=ListBox(windows, height=6, width=35)
+list1=Listbox(window, height=6, width=35)
 list1.grid(row=2, column=0, rowspan=6, columnspan=2)
 
 sb1=Scrollbar(window)
-sb1.grid(row=2,column=1,rowspan=6)
+sb1.grid(row=2,column=2,rowspan=6)
 
-list1.configure(yscrollcomand=sb1.set)
+list1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=list1.yview)
 
-b1=Button(window, text="View All", width=12, command=self.view)
+b1=Button(window, text="View All", width=12, command=bck.view)
 b1.grid(row=2,column=3)
 
-b2=Button(window, text="Search Entry", width=12, command=self.search)
+b2=Button(window, text="Search Entry", width=12, command=bck.search)
 b2.grid(row=3,column=3)
 
-b3=Button(window, text="Add Entry", width=12, command=self.insert)
+b3=Button(window, text="Add Entry", width=12, command=bck.insert)
 b3.grid(row=4,column=3)
 
-b4=Button(window, text="Update Selected", width=12, command=self.update)
+b4=Button(window, text="Update Selected", width=12, command=bck.update)
 b4.grid(row=5,column=3)
 
-b5=Button(window, text="Delete Selected", width=12, command=self.delete)
+b5=Button(window, text="Delete Selected", width=12, command=bck.delete)
 b5.grid(row=6,column=3)
 
-b6=Button(window, text="View All", width=12, command=self.view)
+b6=Button(window, text="View All", width=12, command=bck.view)
 b6.grid(row=7,column=3)
 
+window.mainloop()
